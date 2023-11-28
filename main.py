bda984232017c53235fb30ccbdc64adbe5d86529
Wattisforpower <ethanbarrett11@gmail.com>
2023-11-28 14:42:13 +0000

    Trained Model
    Updates:
    - Added data
    - training a CNN network
    To do:
    - needs finetuning

--- main.py
+++ main.py
@@ -1,2 +1,127 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
 
+from tensorflow import keras
 
+import pathlib
+
+DataDirectiory = pathlib.Path('Data').with_suffix('')
+
+image_count = len(list(DataDirectiory.glob('*/*.jpeg')))
+
+# print(image_count) #51
+
+#create a dataset
+
+batchsize = 32
+image_height = 200
+image_width = 200
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    DataDirectiory,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(image_height, image_width),
+    batch_size=batchsize
+)
+
+validation_ds = tf.keras.utils.image_dataset_from_directory(
+    DataDirectiory,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(image_height, image_width),
+    batch_size=batchsize
+)
+
+class_names = train_ds.class_names
+
+#print(class_names)
+
+# Configure the dataset for performance
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(10).prefetch(buffer_size = AUTOTUNE)
+validation_ds = validation_ds.cache().prefetch(buffer_size = AUTOTUNE)
+
+# Normalization
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_Image = image_batch[0]
+
+#print(np.min(first_Image), np.max(first_Image))
+
+num_classes = len(class_names)
+
+data_augmentation = tf.keras.Sequential(
+    [
+        tf.keras.layers.RandomFlip(
+            "horizontal",
+            input_shape = (image_height, image_width, 3)
+        ),
+        tf.keras.layers.RandomRotation(0.1),
+        tf.keras.layers.RandomZoom(0.1)
+    ]
+)
+
+model = tf.keras.Sequential([
+    data_augmentation,
+    tf.keras.layers.Rescaling(1./255, input_shape=(image_height, image_width, 3)),
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+model.summary()
+
+epochs = 60
+
+history = model.fit(
+    train_ds,
+    validation_data=validation_ds,
+    epochs=epochs
+)
+
+# Visualizing Training Results
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
