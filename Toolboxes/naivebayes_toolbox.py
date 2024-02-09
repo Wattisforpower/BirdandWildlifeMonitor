d0049daad22a17ffde635eba8ce4362183181a4f
Ethan Barrett <65559808+Wattisforpower@users.noreply.github.com>
2024-02-09 11:22:13 +0000

    almost crashed the uni pcs...

--- Toolboxes/naivebayes_toolbox.py
+++ Toolboxes/naivebayes_toolbox.py
@@ -8,12 +8,32 @@ class NaiveBayes:
         pass
 
     def LoadData(self, source) -> None:
+        print("Loading Data")
         # Load the Data
         self.data = pd.read_csv(source, low_memory=False)
+        print("Loaded Data")
 
         # Transpose the data
         self.data = self.data.T
         self.classnames = self.data.pop(400776)
 
-    def SeperateClasses(self) -> None:
+        print(self.data.head())
 
+    def SeperateClasses(self) -> None:
+        classgroups = self.data.groupby(400775)
+
+        self.Group_One = classgroups.get_group(1)
+        self.Group_Two = classgroups.get_group(2)
+        self.Group_Three = classgroups.get_group(3)
+        self.Group_Four = classgroups.get_group(4)
+        self.Group_Five = classgroups.get_group(5)
+        self.Group_Six = classgroups.get_group(6)
+        self.Group_Seven = classgroups.get_group(7)
+        self.Group_Eight = classgroups.get_group(8)
+        self.Group_Nine = classgroups.get_group(9)
+        self.Group_Ten = classgroups.get_group(10)
+
+        print(self.Group_One.head())
+    
+    def __mean(self, num)
+        
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-from Toolboxes import Predictor_Toolbox as PT
-from Toolboxes import Buffer_Toolbox as BT
-from Toolboxes import Data_Transmission_Toolbox as DTT
-from Toolboxes import Neural_Network_Toolbox as NTT
+#from Toolboxes import Predictor_Toolbox as PT
+#from Toolboxes import Buffer_Toolbox as BT
+#from Toolboxes import Data_Transmission_Toolbox as DTT
+#from Toolboxes import Neural_Network_Toolbox as NTT
+from Toolboxes import naivebayes_toolbox as Naive
 #from spidev import SpiDev
 #import wiringpi
-import time
+#import time
+
+NaiveBayes = Naive.NaiveBayes()
 
 #wiringpi.wiringPiSetupSys()
 
-system = PT.RunPredictor()
-Data_Transmit = DTT.TransmissionSystem()
-Network = NTT.Neural_Networks()
+#system = PT.RunPredictor()
+#Data_Transmit = DTT.TransmissionSystem()
+#Network = NTT.Neural_Networks()
 
 
 #spi = SpiDev()
-Buffers = BT.Buffer(520, 20, 20000)
+#Buffers = BT.Buffer(520, 20, 20000)
 
 # Arbitary Pin Value, Change it later!
 Pin = 6
 
 def main():
-	system.GenerateConfusion()
+	NaiveBayes.LoadData('Datasets/dataset2.csv')
+	NaiveBayes.SeperateClasses()
 
 
 	#Result, _ = system.runClassifier('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Audio/Barnswallow/SplitData/BarnSwallow6_split_1.wav', False)
