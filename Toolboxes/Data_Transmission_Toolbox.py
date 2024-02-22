9ca38b627752b989cbf3828b779dba6b6500ec77
Ethan Barrett <65559808+Wattisforpower@users.noreply.github.com>
2024-02-22 10:17:38 +0000

    updates

--- Toolboxes/Data_Transmission_Toolbox.py
+++ Toolboxes/Data_Transmission_Toolbox.py
@@ -1,12 +1,12 @@
 from serial import Serial
 
 class TransmissionSystem:
-	def __init__(self):
-		pass
-	
-	def website(self, result):
-		with open('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Toolboxes/Website/data.csv', 'a') as datafile:
-			datafile.write(str(result) + "\n")
-			
-			datafile.close()
-	
+    def __init__(self):
+        pass
+
+    def website(self, result):
+        with open('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Toolboxes/Website/data.csv', 'a') as datafile:
+            datafile.write(str(result) + "\n")
+            
+            datafile.close()
+
--- main.py
+++ main.py
@@ -21,42 +21,42 @@ Buffers = BT.Buffer(520, 20, 20000)
 Pin = 6
 
 def main():
-	#WB.run()
+    #WB.run()
+    
+
+    Result, _ = system.runClassifier('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Audio/Barnswallow/SplitData/BarnSwallow1_split_2.wav', False)
+    Data_Transmit.website(Result)
+    #system.GenerateConfusion()
+    
+    #print(Result)
+    
+    # Send Classification Result to the website
+    #Data_Transmit.website(Result)
+    
+    '''
+    wiringpi.pinMode(Pin, 0) # Pin 6 Input Pin
+    while True:
+        if wiringpi.digitalRead(Pin) == 1:
+            # Open SPI Port and Read the Data into the Buffers
+            spi.open(5, 1)
+            spi.max_speed_hz = 5000
+            Buffers.BufferLoad(spi.read_all())
+            Buffers.BufferLoad(spi.read_all())
+            spi.close()
+            
+            # Run the Classifier
+            Result, _ = system.runClassifier('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Audio/Barnswallow/SplitData/BarnSwallow6_split_1.wav', False)
+            
+            # Send Classification Result to the website
+            Data_Transmit.website(Result)
+            
+                
+            
+        else:
+            time.sleep(0.1)
+            
+    '''
 
 
-	#Result, _ = system.runClassifier('Audio/Barnswallow/SplitData/BarnSwallow6_split_1.wav', False)
-	system.GenerateConfusion()
-	
-	#print(Result)
-	
-	# Send Classification Result to the website
-	#Data_Transmit.website(Result)
-	
-	'''
-	wiringpi.pinMode(Pin, 0) # Pin 6 Input Pin
-	while True:
-		if wiringpi.digitalRead(Pin) == 1:
-			# Open SPI Port and Read the Data into the Buffers
-			spi.open(5, 1)
-			spi.max_speed_hz = 5000
-			Buffers.BufferLoad(spi.read_all())
-			Buffers.BufferLoad(spi.read_all())
-			spi.close()
-			
-			# Run the Classifier
-			Result, _ = system.runClassifier('/home/pi/Documents/GitHub/BirdandWildlifeMonitor/Audio/Barnswallow/SplitData/BarnSwallow6_split_1.wav', False)
-		
-			# Send Classification Result to the website
-			Data_Transmit.website(Result)
-			
-			
-		
-		else:
-			time.sleep(0.1)
-			
-		'''
-	
-	
-	
 if __name__ == "__main__":
-	main()
+    main()
